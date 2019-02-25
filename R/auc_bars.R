01eb03b6e82dfa0a2d5c8fb6b878de70bfce2e2f
PeerChristensen <hr.pchristensen@gmail.com>
2019-02-25 13:27:47 +0100

    1st commit

---
+++ R/auc_bars.R
@@ -0,0 +1,53 @@
+#' Function for plotting AUC bars
+#' @param H2OAutoML_object provided by user
+#'
+#' @export
+#' @import tidyverse
+#'
+#'
+
+auc_bars <- function(H2OAutoML_object, save_png = F) {
+
+  models <- as.vector(as.character(H2OAutoML_object@leaderboard$model_id)) %>%
+    map(h2o.getModel)
+
+  df <- tibble()
+
+  for (i in 1:length(models)) {
+
+    perf <- h2o.performance(models[[i]], test_hf)
+    auc  <- perf@metrics$AUC
+
+    model_id  <- models[[i]]@model_id
+    algorithm <- models[[i]]@algorithm
+
+    d <- tibble(model_id,algorithm,auc)
+    d <- add_column(d, model_rank = i)
+
+    df <- rbind(df,d)
+  }
+
+  df$model_id <- str_split(df$model_id, "_AutoML") %>%
+    map_chr(1) %>%
+    paste0(df$model_rank,": ",.)
+
+  p <- df %>%
+    ggplot(aes(model_id, auc, fill = model_id)) +
+    geom_col() +
+    xlab('Models') +
+    ylab('AUC') +
+    scale_fill_viridis_d(guide=F) +
+    geom_text(aes(label=round(auc,3)), position=position_dodge(width=0.9), vjust=-0.9, size = 6) +
+    ggtitle('AUC of the best models') +
+    theme(plot.title = element_text(size = 16),
+          axis.title = element_text(size = 12),
+          axis.text.x  = element_text(size = 12,angle = 45, hjust = 1),
+          axis.text.y  = element_text(size = 12)) +
+    ylim(c(0,1))
+
+  print(p)
+
+  if (save_png == T) {
+    ggsave("auc_bars.png")
+  }
+}
---
+++ R/roc_curves.R
@@ -0,0 +1,72 @@
+#' Functions for plotting roc curves
+#'
+#' @param H2OAutoML_object provided by user
+#'
+#' @export
+#' @import dplyr
+#' @import purrr
+#' @import ggplot2
+#'
+### function for preparing ROC curves for plotting
+
+roc_curves <- function(H2OAutoML_object, plot = T, best = F, save_png = F) {
+
+  if (best == T) {
+    models <- as.vector(as.character(H2OAutoML_object@leader@model_id)) %>%
+      map(h2o.getModel)
+  }
+  else {
+    models <- as.vector(as.character(H2OAutoML_object@leaderboard$model_id)) %>%
+      map(h2o.getModel)
+  }
+
+  df <- tibble()
+
+  for (i in 1:length(models)) {
+
+    perf <- h2o.performance(models[[i]], test_hf)
+    tpr  <- perf@metrics$thresholds_and_metric_scores$tpr
+    fpr  <- perf@metrics$thresholds_and_metric_scores$fpr
+
+    model_id  <- models[[i]]@model_id
+    algorithm <- models[[i]]@algorithm
+
+    d <- tibble(model_id,algorithm,tpr,fpr)
+    d <- add_row(d,model_id = model_id, algorithm=algorithm,tpr=0,fpr=0,.before=T)
+    d <- add_row(d,model_id = model_id, algorithm=algorithm,tpr=0,fpr=0,.before=F)
+    d <- add_column(d, model_rank = i)
+
+    df <- rbind(df,d)
+  }
+
+  df$model_id <- str_split(df$model_id, "_AutoML") %>%
+    map_chr(1) %>%paste0(df$model_rank,": ",.)
+
+  if (plot == T) {
+    p <- df %>%
+      ggplot(aes(fpr,tpr,colour = model_id)) +
+      geom_line(size = 1,alpha=.8) +
+      geom_segment(aes(x=0,y=0,xend = 1, yend = 1),linetype = 2,col='grey', size =1) +
+      coord_fixed() +
+      xlab('False Positive Rate') +
+      ylab('True Positive Rate') +
+      ggtitle('ROC curves',
+              subtitle = "Comparison of the best models") +
+      theme(plot.title    = element_text(size = 16),
+            plot.subtitle = element_text(size = 12,face="italic",vjust=-1)) +
+      scale_colour_viridis_d("Models")
+
+    print(p)
+
+    if (save_png == T) {
+      ggsave("roc_curves.png")
+    }
+
+    return(df)
+
+  }
+  else {
+
+    return(df)
+  }
+}
